da9c124a3b3e9e8be347223605dfeb6a3f3ca3b8
skelton <skelton25@gmail.com>
2018-07-17 20:53:53 +0200

    Ps3 mode by default

--- drivers/input/rocker/adc_js.c
+++ drivers/input/rocker/adc_js.c
@@ -1,3 +1,31 @@
+*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * version V4.3
+ * compatible PS3 and XBOX mode
+ * change right stick range from -1 to 1
+ * fixed self-test suspend release
+ * add key and joystick self-test
+ * add early suspend 
+ * add hot key mirco
+ * change to X-input mode
+ * add exchange L1/L2 and R1/R2
+ */
+
 
 #include <linux/module.h>
 #include <linux/init.h>
@@ -59,7 +87,7 @@
 //#endif
 
 //default mode, 0 is PS3 mode, 1 is XBOX mode
-int ioctl_xbox = 1;
+int ioctl_xbox = 0;
 //default exchange, 0 is exchange, 1 is default 
 int ioctl_exchange = 1;
 
